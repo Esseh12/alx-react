fa0c1040b7d25b266e8165f580bc6899114a4a80
Esseh12 <gbenmoese788@gmail.com>
2024-08-20 00:00:37 +0100

    deleted files by mistake

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -4,22 +4,7 @@ module.exports = {
     mode: "production",
     entry: {
      main: path.resolve(__dirname, './js/dashboard_main.js'), 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)/$i,
-        type: 'asset/resource',
-      }
-    ]
-  },
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
