ce61b7b84e15dbebaec8687a2256f5d409d11bec
Esseh12 <gbenmoese788@gmail.com>
2024-08-19 17:08:57 +0100

    webpack config

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,14 +1,12 @@
 const path = require("path");
 
-const config = {
+module.exports = {
     mode: "production",
     entry: {
      main: path.resolve(__dirname, './js/dashboard_main.js'), 
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+        filename: 'bundle.js',
   },
 };
-
-module.exports = config;
