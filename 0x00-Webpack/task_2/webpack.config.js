0213e23830acb7856e395d2a4b124d9f4553aea6
Esseh12 <gbenmoese788@gmail.com>
2024-08-19 22:43:15 +0100

    css loader

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -4,6 +4,14 @@ module.exports = {
     mode: "production",
     entry: {
      main: path.resolve(__dirname, './js/dashboard_main.js'), 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","csss-loader"],
+        }
+      ],
     },
     output: {
         path: path.resolve(__dirname, 'public'),
