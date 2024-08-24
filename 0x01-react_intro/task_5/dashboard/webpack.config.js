cf617f862243e837ee521c9db950eb1fc2227a4f
Esseh12 <gbenmoese788@gmail.com>
2024-08-24 01:27:16 +0100

    config file

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,64 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'development', // or 'production'
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'bundle.js',
   },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    hot: true,
-    open: true,
-  },
+  devtool: 'inline-source-map', // Enable inline source maps
   module: {
     rules: [
       {
-        test: /\.jsx?$/, // This will match both .js and .jsx files
-        exclude: /node_modules/, // Do not transpile node_modules
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'], // Presets for modern JavaScript and React
-          },
-        },
+        test: /\.css$/, // Apply to .css files
+        use: [
+          'style-loader', // Injects CSS into the DOM
+          'css-loader',   // Resolves CSS imports
+        ],
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg)$/, // Apply to image files
         use: [
           {
-            loader: 'file-loader',
+            loader: 'file-loader', // Emits files and returns the URL
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
+              name: '[name].[ext]', // Keep the original name and extension
+              outputPath: 'images/', // Output to the images folder
             },
           },
           {
-            loader: 'image-webpack-loader',
+            loader: 'image-webpack-loader', // Optimize images
             options: {
               bypassOnDebug: true,
-              disable: true,
+              disable: true, // Set to false to enable optimization
             },
           },
         ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Resolve .js and .jsx extensions
-  },
-  devtool: 'inline-source-map',
   plugins: [
+    new CleanWebpackPlugin(), // Clean the output folder before each build
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'dist/index.html'),
-      inject: 'body',
+      template: './src/index.html', // Use 'index.html' as a template
     }),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 8564,
+    hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',  // This will apply code splitting to all chunks
+    },
+  },
 };
