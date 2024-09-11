0102e6810030e7f8c7cdd590b489f50ce024f4cc
Esseh12 <gbenmoese788@gmail.com>
2024-09-11 15:11:31 +0100

    fromJs of the immutable library

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -4,4 +4,12 @@ const getImmutableObject = (object) => {
     return Immutable.fromJS(object);
 }
 
+const object = {
+     fear: true,
+     smell: -1033575916.9145899,
+     wall: false,
+     thing: -914767132
+}
+
+console.log(getImmutableObject(object));
 module.exports = getImmutableObject;
