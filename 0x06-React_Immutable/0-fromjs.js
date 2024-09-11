e79678b1f99b01be8c53257667679edee63f890c
Esseh12 <gbenmoese788@gmail.com>
2024-09-11 15:04:30 +0100

    immutable map using fromJs

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -5,10 +5,3 @@ const getImmutableObject = (object) => {
 }
 
 module.exports = getImmutableObject;
-
-getImmutableObject({
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-});
