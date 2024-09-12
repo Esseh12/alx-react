0ed8559ae55c01bec3919f411542bb8461148c41
Esseh12 <Gbenmoese788@gmailcom>
2024-09-12 23:18:36 +0100

    updated .gitignore file

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,14 @@
-import { fromJS } from 'immutable';
+const Immutable = require('immutable');
 
-export default function getImmutableObject(object) {
-  return fromJS(object);
+const getImmutableObject = (object) => {
+  return Immutable.fromJS(object)
 }
+
+const obj = {
+     fear: true,
+     smell: -1033575916.9145899,
+     wall: false,
+     thing: -914767132
+};
+
+console.log(getImmutableObject(obj));
